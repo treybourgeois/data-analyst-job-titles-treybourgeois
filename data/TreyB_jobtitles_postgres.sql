bc024335fc3390766154f99eddcd130552274f88
treybourgeois <trey@retroactivepedals.com>
2021-02-20 13:48:17 -0600

    Final Push After Walkthrough

--- data/TreyB_jobtitles_postgres.sql
+++ data/TreyB_jobtitles_postgres.sql
@@ -12,10 +12,10 @@ SELECT COUNT (*)
 FROM data_analyst_jobs
 WHERE location = 'TN'; --42
 
-SELECT COUNT (*)
+SELECT COUNT (location)
 FROM data_analyst_jobs
 WHERE location ='TN' 
-OR location = 'KY'; -- 54
+OR location = 'KY'; -- 54 TRY WHERE LOCATION IN ('TN','KY')
 
 -- 4.)
 SELECT COUNT (*)
@@ -24,17 +24,18 @@ WHERE location = 'TN'
 AND star_rating >4; -- 6
 
 -- 5.)
-SELECT COUNT (*)
+SELECT COUNT (REVIEW_COUNT)
 FROM data_analyst_jobs
-WHERE review_count >=500
-OR review_count <=1000; -- 3072
+WHERE review_count>=500
+AND review_count<=1000; -- 3072 CAN USE BETWEEN HERE AS WELL
 
 -- 6.)
-SELECT location AS state, company, ROUND(AVG(star_rating),1) AS avg_rating
+SELECT location AS state, ROUND(AVG(star_rating),2) AS avg_rating
 FROM data_analyst_jobs
 WHERE star_rating IS NOT NULL
-GROUP BY location, company, star_rating
-ORDER BY star_rating DESC; -- California
+and location Is NOT NULL
+GROUP BY location
+ORDER BY avg_rating DESC; -- California
 
 -- 7.)
 SELECT COUNT(DISTINCT title)
@@ -44,32 +45,31 @@ FROM data_analyst_jobs; --881
 SELECT COUNT(DISTINCT title) AS unique_titles, location AS state
 FROM data_analyst_jobs
 WHERE location ='CA'
-GROUP BY location, title
+GROUP BY location
 ORDER BY location; -- 230
 
 -- 9.)/10.)
-SELECT DISTINCT company , ROUND(AVG(star_rating), 2) AS avg_rating, review_count, location AS state
+SELECT DISTINCT company , ROUND(AVG(star_rating), 2) AS avg_rating
 FROM data_analyst_jobs
 WHERE company IS NOT NULL
 AND review_count >5000
-GROUP BY company, star_rating, review_count, location -- 82 companies >5000 reviews
+GROUP BY company -- 82 companies >5000 reviews
 ORDER BY avg_rating DESC; -- Am Express, Gen. Motors, Kaiser Permanente, Microsoft,Unilever all tied with 4.20??
 
 -- 11.)
-SELECT COUNT (*) title
+SELECT COUNT(DISTINCT(title))
 FROM data_analyst_jobs
-WHERE title LIKE '%Analyst' 
-OR title LIKE 'Analyst%'; -- 2166 -- why does '%Analyst%' return more??
+WHERE title iLIKE '%Analyst%'; -- 2166 -- why does '%Analyst%' return more??
 
 -- 12.)
 SELECT DISTINCT title
 FROM data_analyst_jobs
 WHERE title NOT iLIKE '%Analyst%'
 AND title NOT iLIKE '%Analytics%'
-ORDER BY title;
+ORDER BY title; -- tableau
 
 -- BONUS
-SELECT domain AS industry, COUNT(title) AS hard_to_fill
+SELECT domain AS industry, COUNT(title)/2 AS hard_to_fill
 FROM data_analyst_jobs
 WHERE domain IS NOT NULL
 AND skill iLIKE '%SQL%'
